0d967d23091f78373f827ab36705ae21a8984ac1
Hope_Maglaqui <maglaquijohnhopefuedan@gmail.com>
2024-05-12 19:32:01 +0800

    mobile

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,6 +3,10 @@ import React,{useState,useEffect} from 'react'
 import './css/nav.css'
 // icons
 import { TbPawFilled } from "react-icons/tb";
+import { IoHomeOutline,IoPawOutline,IoSearchSharp,IoBagAddOutline  } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { FaBars } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
 // assets
 import logoname from './assets/logoname.png'
 import Usermodalsign from './pages/modals/signInuser'
@@ -69,34 +73,56 @@ const logoutbtn = () => {
   return (
     <div className={`navMain ${hasShadow ? 'shadow' : ''}`}>
         <nav>
-            <div className={`logsignModal ${openSignin}`}>
-                <Usermodalsign onOpensignin={setOpensignin} onOpenCreate={'signin'}/>
-            </div>
-            <div className="nav-container">
-                <div className="mav-contents">
-                    <div className="navlogo">
-                        <img src={logoname} alt="" />
-                    </div>
-                    <div className="signUpnav">
-                        <ul>
-                            <li>statistics</li>
-                            <li>showcase</li>
-                            <li>products</li>
-                            <li>about us</li>
+          <div className={`logsignModal ${openSignin}`}>
+              <Usermodalsign onOpensignin={setOpensignin} onOpenCreate={'signin'}/>
+          </div>
+          <div className="nav-container">
+              <div className="mav-contents">
+                  <div className="navlogo">
+                      <img src={logoname} alt="" />
+                  </div>
+                  <div className="signUpnav">
+                      <ul>
+                          <li>statistics</li>
+                          <li>showcase</li>
+                          <li>products</li>
+                          <li>about us</li>
+                      </ul>
+                      {/* {!logout ? <button onClick={openModalsign}> <TbPawFilled className='signIco'/>sign up</button> :  
+                      <button className='userBtnnav'> <TbPawFilled className='signIco'/>{user.username}
+                        <ul className='userdetList'>
+                          <li>profile</li>
+                          <li>pet</li>
+                          <li onClick={logoutbtn}>Logout</li>
                         </ul>
-                        {!logout ? <button onClick={openModalsign}> <TbPawFilled className='signIco'/>sign up</button> :  
-                        <button className='userBtnnav'> <TbPawFilled className='signIco'/>{user.username}
-                          <ul className='userdetList'>
-                            <li>profile</li>
-                            <li>pet</li>
-                            <li onClick={logoutbtn}>Logout</li>
-                          </ul>
-                        </button>
-                        }
-                    </div>
-                </div>
-            </div>
+                      </button>
+                      } */}
+                  </div>
+              </div>
+          </div>
         </nav>
+        <div className="mobileNav">
+          <div className="mainMobile">
+            <section className='mbLogosearch'>
+              <div className="mbnavlogo">
+                <img src={logoname} alt="" />
+              </div>
+              <div className="mbSearch">
+                <div className="searchInput">
+                  <input type="text" /><IoSearchSharp className='mbIconsearch'/>
+                </div>
+              </div>
+            </section>
+            <section className='mbNavicons'>
+              <IoHomeOutline className='mbIcons'/>
+              <IoPawOutline className='mbIcons'/>
+              <IoBagAddOutline className='mbIcons'/>
+              <FiShoppingCart className='mbIcons'/>
+              <CgProfile className='mbIcons'/>
+              <FaBars className='mbIcons'/>
+            </section>
+          </div>
+        </div>
     </div>
   )
 }
--- src/components/pages/introHome.js
+++ src/components/pages/introHome.js
@@ -1,15 +1,31 @@
 import React,{useState,useEffect} from 'react'
 // css
 import '../css/introHome.css'
+// icons
+import { PiHeartStraightBold,PiHeartStraightFill  } from "react-icons/pi";
+import { MdOutlineModeComment } from "react-icons/md";
+import { LuSend } from "react-icons/lu";
+import { IoMdSend } from "react-icons/io";
 // assets
 import pawLogo from '../assets/logoname.png'
 import shopcat from '../assets/intro.png'
 import apple from '../assets/purr-apple.png'
 import fish from '../assets/purr-fish-5.png'
 import jar from '../assets/purr-jar-with-cookies.png'
+import prof from '../assets/notes-male-avatar.png'
+import stupidcat from '../assets/purr-online-shopping.png'
+import why from '../assets/why.png'
+import spyCat from '../assets/purr-cat-sitting-and-playing-with-a-spider.png'
 
 const Introhome = ({ onOpensignin }) => {
   const [localCurbuyer, setLocalcurbuyer] = useState();
+
+  const [likePost, setLikepost] = useState(false)
+
+  const likeftPost = () => {
+      setLikepost(likePost=>!likePost)
+  }
+
 
   useEffect(() => {
     handlelocalBuyer();
@@ -52,6 +68,340 @@ const Introhome = ({ onOpensignin }) => {
             <section>
               <img src={shopcat} alt="" className="shoopCat" />
             </section>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>Whether your pet prefers kibble, wet food, or treats, we offer a wide range of options to suit every taste and dietary requirement.</p>
+              </div>
+              <div className="mbpost">
+                <img src={stupidcat} alt="" />
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>buy my products, im in debt</p>
+              </div>
+              <div className="mbpost">
+                <img src={why} alt="" />
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>Whether your pet prefers kibble, wet food, or treats, we offer a wide range of options to suit every taste and dietary requirement.</p>
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>We handpick only the finest ingredients for our pet food, ensuring that your furry friend gets the nutrition they deserve.</p>
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>Our products are formulated by pet nutrition experts to promote overall health and vitality, supporting your pet's well-being from nose to tail.</p>
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="sampAddpip">
+            <div className="sampips">
+              <div className="sampipsheader">
+                <h1>look for adorable babies</h1>
+                <section>
+                  <div className="samPetscard">
+                    <div className="sPetspf">
+                      <img src={spyCat} alt="" />
+                    </div>
+                    <h1>cat rodriguez</h1>
+                    <div className="sampAdd">
+                      <p>add fur-baby</p>
+                    </div>
+                  </div>
+                  <div className="samPetscard">
+                    <div className="sPetspf">
+                      <img src={spyCat} alt="" />
+                    </div>
+                    <h1>cat rodriguez</h1>
+                    <div className="sampAdd">
+                      <p>add fur-baby</p>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>Say goodbye to last-minute trips to the store! With Pawsome, you can order your pet's favorite food from the comfort of your home and have it delivered straight to your doorstep.</p>
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="mbsamplePost">
+            <div className="samplepostHeader">
+              <div className="mbProf">
+                <img src={prof} alt="" />
+              </div>
+              <div className="mbsp-name">
+                <section>
+                  <h1>Nathaniel</h1>
+                  <p>24hrs ago</p>
+                </section>
+              </div>
+            </div>
+            <div className="mbSamppost">
+              <div className="mbsampCap">
+                <p>We believe that every pet deserves the best without breaking the bank. That's why we offer competitive prices and regular discounts to make caring for your pet more accessible.</p>
+              </div>
+              <div className="mbsampostIcons">
+                <div className="mbsampReacts">
+                  {!likePost ? (
+                    <PiHeartStraightBold
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  ) : (
+                    <PiHeartStraightFill
+                      className={`sampPostlike ${likePost}`}
+                      onClick={likeftPost}
+                      id='sampReacts'
+                    />
+                  )}
+                  <MdOutlineModeComment className="sampcommentFtpost" id='sampReacts'/>
+                  <LuSend className="sampshareFtpost" id='sampReacts'/>
+                </div>
+                <div className="msampComment">
+                  <section>
+                    <input type="text" />
+                    <IoMdSend className='mbSendcomment'/>
+                  </section>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
